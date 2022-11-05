508cddf8954f2f5aea4e03e792ff4ff75eae7535
Solovijovas <guisolovijovas@gmail.com>
2022-11-05 17:09:57 -0300

    DIVERSAS MUDANCAS como calculo de integral

--- SignalAnalysis.py
+++ SignalAnalysis.py
@@ -1,11 +1,13 @@
-import math
 import numpy as np
 import PySimpleGUI as sg
 import matplotlib.pyplot as plt
+import time
 import ctypes
-import os
+import random
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from scipy.signal import find_peaks
+from scipy import integrate as intg
+from math import sqrt, ceil
 
 # Functions to prevent GUI blurring
 def make_dpi_aware():
@@ -17,7 +19,7 @@ def make_dpi_aware():
 make_dpi_aware()
 
 # Function for drawing
-def draw_figure_w_toolbar(canvas, figure, canvas_toolbar):
+def draw_figure_w_toolbar(canvas, fig, canvas_toolbar):
     if canvas.children:
         for child in canvas.winfo_children():
             child.destroy()
@@ -35,7 +37,7 @@ def clean_graphs():
     ax1.cla()
     ax2.cla()
     ax1.set_ylabel('Tensão(Vrms)')
-    ax1.set_xlabel('Tempo (s)')
+    ax1.set_xlabel('Tempo (ms)')
     ax2.set_ylabel('Tensão(Vrms)')
     ax2.set_xlabel('Frequência (Hz)')
     window['-ML1-'].update('')
@@ -110,7 +112,7 @@ entradas = [[sg.Text('ANALISE DE SINAIS')],
             [sg.Push(), sg.T('Harmonica 4 (Hz):'), sg.Input(enable_events=True, key='-harmonica4-', size=(3, 1))],
             [sg.Push(), sg.T('Harmonica 5 (Hz):'), sg.Input(enable_events=True, key='-harmonica5-', size=(3, 1))],
             [sg.Push(), sg.T('Tensão (Vrms):'), sg.Input(enable_events=True, key='-amplitude-', size=(3, 1))],
-            [sg.Button('Plotar gráfico'), sg.Button('Limpar')],[sg.Multiline(size=(2, 10), key='-ML1-', expand_x=True, expand_y=True, no_scrollbar=True)]]
+            [sg.Button('Plotar gráfico'), sg.Button('Limpar')],[sg.Multiline(size=(10, 15), key='-ML1-', expand_x=True, expand_y=True, no_scrollbar=True)]]
 # Layout creation
 graficos = [[sg.Canvas(key='controls_cv', expand_x=True, expand_y=True)],[sg.Canvas(key='-CANVAS-', expand_x=True, expand_y=True)],
             [sg.Button('Importar',expand_x=True, expand_y=True), sg.Button('Exportar',expand_x=True, expand_y=True),
@@ -123,19 +125,14 @@ window = sg.Window('ANALISE DE SINAIS', layout, size=(screensize), finalize=True
                    element_justification='left', font='Monospace 10', resizable=True)
 window.Maximize()
 # Create a fig for embedding.
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(11, 5))
 
 # Associate fig with Canvas.
 clean_graphs()
 
 #Variaveis globais
-t_global = ''
-s_global = ''
-file_global = ''
-s_import_array = ''
-t_import_array = ''
-frequencias_global = ''
-amplitudes_global = ''
+t_global, s_global, file_global, s_import_array, t_import_array, frequencias_global, amplitudes_global = '','','','','','',''
+num_pontos, ponto_parada  = 10000, 0.5 
 
 # Event loop
 def main():
@@ -146,62 +143,64 @@ def main():
             break
 
         elif event == "Plotar gráfico":
+            start_counter_ns = time.perf_counter_ns()
             clean_graphs()
             validation_result = validate(values)
             if validation_result[0]:
-                ha1 = int(values['-harmonica1-'])
-                ha2 = int(values['-harmonica2-'])
-                ha3 = int(values['-harmonica3-'])
-                ha4 = int(values['-harmonica4-'])
-                ha5 = int(values['-harmonica5-'])
+                ha1, ha2, ha3, ha4, ha5 = int(values['-harmonica1-']), int(values['-harmonica2-']), int(values['-harmonica3-']), int(values['-harmonica4-']), int(values['-harmonica5-'])
                 a1 = int(values['-amplitude-'])
-                a2 = a1/3
-                a3 = a2/3
-                a4 = a3/3
-                a5 = a4/3
-                ponto_parada = 0.1
-                num_pontos = 1000
+                a2 = random.uniform(1, a1/2)
+                a3 = random.uniform(1, a2/2)
+                a4 = random.uniform(1, a3/2)
+                a5 = random.uniform(1, a4/2)
+                sample_rate = num_pontos/ponto_parada
                 t = np.linspace(0, ponto_parada, num_pontos)
-                t_global = t
-                s = a1 * np.sin(ha1 * 2 * np.pi * t) + a2 * np.sin(ha2 * 2 * np.pi * t) + a3 * np.sin(ha3 * 2 * np.pi * t) + a4 * np.sin(ha4 * 2 * np.pi * t) + a5 * np.sin(ha5 * 2 * np.pi * t)
-                s_global = s
-                grafico1 = ax1.plot(t, s)
+                s = a1*np.sin(ha1*2*np.pi*t)+a2*np.sin(ha2*2*np.pi*t)+a3*np.sin(ha3*2*np.pi*t)+a4*np.sin(ha4*2*np.pi*t)+a5*np.sin(ha5*2*np.pi*t)
                 fft = np.fft.fft(s)
-                T = t[1] - t[0]
+                T = t[1]-t[0]
                 N = s.size
                 f = np.fft.fftfreq(len(s), T)
                 frequencias = f[:N // 2]
-                frequencias_global = frequencias
                 amplitudes = np.abs(fft)[:N // 2] * 1 / N
+                t_global = t
+                s_global = s
+                frequencias_global = frequencias
                 amplitudes_global = amplitudes
                 peaks,_ = find_peaks(amplitudes)
-                harm_freq_list = []
-                harm_ampli_list = []
-
+                harm_freq_list, harm_ampli_list = [], []
+                
                 for i in range(len(peaks)):
                     harm_freq_list.append(frequencias[peaks[i]])
+                    harm_ampli_list.append(amplitudes[peaks[i]])
 
-                for j in range(len(peaks)):
-                    harm_ampli_list.append(amplitudes[peaks[j]])
-
-                print(harm_freq_list)
+                periodo = 1/harm_freq_list[0]
+                t_index = np.where(t == periodo)[0]
+                t_index_half = int(t_index[0]/2)
+                print(t_index_half)
                 sum_all = sum_of_squares(harm_ampli_list)
-                max_freq = max(harm_freq_list)
-                max_ampli = max(harm_ampli_list)
-                THD = (math.sqrt((sum_all-(max_ampli ** 2))/max_ampli ** 2))*100
-                TDD = (math.sqrt((sum_all - (max_ampli ** 2)) / max_ampli ** 2)) * 100
-                grafico2 = ax2.bar(frequencias, amplitudes)
-                ax2.set_xlim([0, max_freq+15])
+                max_freq, max_ampli = max(harm_freq_list), max(harm_ampli_list)
+                THD = (sqrt((sum_all-(max_ampli**2))/max_ampli**2))*100
+                TDD = (sqrt((sum_all-(max_ampli**2))/max_ampli**2))*100
+                TotalInt = 2 * abs(intg.trapezoid(t[0:t_index_half], s[0:t_index_half]))
+                print(TotalInt)
+                grafico1 = ax1.plot(t, s)
+                grafico2 = ax2.plot(frequencias, amplitudes)
+                ax1.grid(True)
+                ax2.grid(True)
+                ax1.set_xlim([0, periodo])
+                ax2.set_ylim([0, max_ampli+10])
+                ax2.set_xlim([0, max_freq+10])
                 window['-ML1-'].print('THD é: {:0.1f}%'.format(THD))
                 window['-ML1-'].print('TDD é: {:0.1f}%'.format(TDD))
-                window['-ML1-'].print('Maior frequencia:',max_freq)
-                for i in range(len(harm_freq_list)):
-                    window['-ML1-'].print('Harmonica {}: {}Hz'.format(i+1,math.ceil(harm_freq_list[i])))
-                
+                for i in range(len(harm_freq_list)) and range(len(harm_ampli_list)):
+                    window['-ML1-'].print('Harmonica {}: {}Hz, A={:.2f}'.format(i+1,ceil(harm_freq_list[i]),harm_ampli_list[i]))
+                end_counter_ns = time.perf_counter_ns()
+                timer_ns = end_counter_ns - start_counter_ns
+                print('TEMPO DE EXECUCAO', timer_ns/10**9)
 
             else:
                 error_message = generate_error_message(validation_result[1])
-                sg.popup(error_message)
+                sg.popup('Campo incompleto ou nulo!',error_message)
 
         elif event == 'Importar':
             filename = sg.popup_get_file('Will not see this message', no_window=True, multiple_files=False,
@@ -211,11 +210,16 @@ def main():
                 main()
             
             else:
+                with open(filename, 'r') as fp:
+                    num_lines = sum(1 for line in fp)
+                    print('Total lines:', num_lines)
+
                 with open(filename, 'r') as myfile:
                     linhas_t = myfile.readlines()[0:1000]
                     t_import = []
                     for sub in linhas_t:
                         t_import.append(sub.replace("\n", ""))
+                        
                 t_import_array = np.array(t_import)
                 t_import_array_float = np.asarray(t_import_array, dtype=float)
 
@@ -224,9 +228,9 @@ def main():
                     s_import = []
                     for sub in linhas_s:
                         s_import.append(sub.replace("\n", ""))
+
                 s_import_array = np.array(s_import)
                 s_import_array_float = np.asarray(s_import_array, dtype=float)
-                grafico1 = ax1.plot(t_import_array_float, s_import_array_float)
                 fft = np.fft.fft(s_import_array_float)
                 T = t_import_array_float[1] - t_import_array_float[0]
                 N = s_import_array_float.size
@@ -234,22 +238,27 @@ def main():
                 frequencias = f[:N // 2]
                 amplitudes = np.abs(fft)[:N // 2] * 1 / N
                 peaks,_ = find_peaks(amplitudes)
-                harm_freq_list = []
-                harm_ampli_list = []
+                harm_freq_list, harm_ampli_list = [], []
 
                 for i in range(len(peaks)):
                     harm_freq_list.append(frequencias[peaks[i]])
-
-                for j in range(len(peaks)):
-                    harm_ampli_list.append(amplitudes[peaks[j]])
+                    harm_ampli_list.append(amplitudes[peaks[i]])
 
                 sum_all = sum_of_squares(harm_ampli_list)
-                max_freq = max(harm_freq_list)
-                max_ampli = max(harm_ampli_list)
-                THD = (math.sqrt((sum_all - (max_ampli ** 2)) / max_ampli ** 2)) * 100
-                TDD = (math.sqrt((sum_all - (max_ampli ** 2)) / max_ampli ** 2)) * 100
-                grafico2 = ax2.bar(frequencias, amplitudes)
-                ax2.set_xlim([0, max_freq + 15])
+                max_freq, max_ampli = max(harm_freq_list), max(harm_ampli_list)
+                THD = (sqrt((sum_all - (max_ampli ** 2)) / max_ampli ** 2)) * 100
+                TDD = (sqrt((sum_all - (max_ampli ** 2)) / max_ampli ** 2)) * 100
+                grafico1 = ax1.plot(t_import_array_float, s_import_array_float)
+                grafico2 = ax2.plot(frequencias, amplitudes)
+                ax1.grid(True)
+                ax2.grid(True)
+                ax1.set_xlim([0, 1/harm_freq_list[0]])
+                ax2.set_ylim([0, max_ampli+10])
+                ax2.set_xlim([0, max_freq+10])
+                window['-ML1-'].print('THD é: {:0.1f}%'.format(THD))
+                window['-ML1-'].print('TDD é: {:0.1f}%'.format(TDD))
+                for i in range(len(harm_freq_list)) and range(len(harm_ampli_list)):
+                    window['-ML1-'].print('Harmonica {}: {}Hz, A={:.2f}'.format(i+1,ceil(harm_freq_list[i]),harm_ampli_list[i]))
                 
 
         elif event == 'Exportar':
